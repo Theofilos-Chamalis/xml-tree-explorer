07dcf3a2fa853a3f61ce9fff8793c9cc6e695c1e
Theofilos Chamalis <theofxam@gmail.com>
2022-05-15 23:10:59 +0300

    Added vite typescript specific plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsConfigPaths(), checker({ typescript: true })],
   server: {
     port: 3200,
   },
